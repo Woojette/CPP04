8f48209b301a8b2303a7bf34074edf1c106b6c40
Clement Beaurain <clementbeaurainpro@gmail.com>
2026-04-24 14:33:32 +0200

    ex00, 01, 02 fait

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -7,18 +7,38 @@
 
 int main(void)
 {
+    std::cout << "########## create animal class ##########" << std::endl << std::endl;
     const Animal*   meta = new Animal();
-    const Animal*   j = new Dog();
-    const Animal*   i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
+    std::cout << "type: " << meta->getType() << " ( will output the animal sound! )" << std::endl;
+    std::cout << "sound: ";
     meta->makeSound();
 
-    std::cout << "##### Wrong Animal #####" << std::endl;
+    std::cout << std::endl << "########## create animal [dog] class ##########" << std::endl << std::endl;
+    const Animal*   j = new Dog();
+    std::cout << "type: " << j->getType() << " ( will output the dog sound! )" << std::endl;
+    std::cout << "sound: ";
+    j->makeSound(); //will output the dog sound!
+
+    std::cout << std::endl << "########## create animal [cat] class ##########" << std::endl << std::endl;
+    const Animal*   i = new Cat();
+    std::cout << "type: " << i->getType() << " ( will output the cat sound! )" << std::endl;
+    std::cout << "sound: ";
+    i->makeSound(); //will output the cat sound!
+
+    std::cout << std::endl << "########## Wrong Animal ##########" << std::endl << std::endl;
     const WrongAnimal* a = new WrongCat();
     a->makeSound();
+
+    std::cout << std::endl << "########## destructor ##########" << std::endl;
+    std::cout << std::endl << "========== destructor [animal] =========="<< std::endl;
+    delete meta;
+    std::cout << std::endl << "========== destructor [dog] =========="<< std::endl;
+    delete j;
+    std::cout << std::endl << "========== destructor [cat] =========="<< std::endl;
+    delete i;
+    std::cout << std::endl << "========== destructor [wrong animal] =========="<< std::endl;
+    delete a;
+    std::cout << std::endl;
 
     return (0);
 }
